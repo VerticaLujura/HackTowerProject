5c8dbd2f11b4052e0edd9eb0f3640caf290181dc
RYMND1875 <aiminzoku@gmail.com>
2019-11-01 01:45:58 +0900

    ルート設定実装
    仮のユニットも入ってます

---
+++ HackTower/Assets/Game/Maps/TileRootMaker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileRootMaker : MonoBehaviour
+{
+    public Vector3Int StartPosition;
+    public List<Vector3Int> RootList = new List<Vector3Int>();
+  
+    void RootMake(){
+        var tm= this.gameObject.GetComponent<Tilemap>();
+        if(tm.HasTile(StartPosition)==false){
+            Debug.Log("startPositionにタイルが見つかりません");
+        }
+        else {
+            RootList.Add(StartPosition);
+            int n=0;
+            while(true){
+                if(n==RootList.Count){
+                    Debug.Log("root探索終了");
+                    Debug.Log("終端"+RootList[n-1]);
+                    break;
+                    }
+                Debug.Log(n+" "+RootList.Count);
+                void func(Vector3Int gap){
+                    if(tm.HasTile(RootList[n]+gap)){
+                        if(n==0)RootList.Add(RootList[n]+gap);
+                        else if(RootList[n]+gap!=RootList[n-1])RootList.Add(RootList[n]+gap);
+                        }
+                    }
+                    func(new Vector3Int(1,0,0));
+                    func(new Vector3Int(0,1,0));
+                    func(new Vector3Int(-1,0,0));
+                    func(new Vector3Int(0,-1,0));
+                
+            n++;
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        RootMake();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ HackTower/Assets/Game/Units/UnitExample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitExample : MonoBehaviour
+{
+    int selfRootIndex;
+    List<Vector3Int> root;
+    float defaultMoveCoolTime=1;
+    float moveCoolTime=1;
+    int rootIndex=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        root=GameObject.Find("WayTile").GetComponent<TileRootMaker>().RootList;
+        this.gameObject.transform.position=root[0];
+    }
+    void Update(){
+        moveCoolTime-=Time.deltaTime;
+        if(moveCoolTime<0){
+            rootIndex++;
+            if(rootIndex>root.Count)Destroy(gameObject);
+            transform.position=root[rootIndex];
+            moveCoolTime=defaultMoveCoolTime;
+        }
+    }
+
+   
+}
