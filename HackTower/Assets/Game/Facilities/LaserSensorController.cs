5e3ff2d10a048d190bfa9176642582115685e6c1
RYMND1875 <56013293+RYMND1875@users.noreply.github.com>
2019-11-11 16:16:09 +0900

    謎センサー/unit当たり判定追加

---
+++ HackTower/Assets/Game/Facilities/LaserSensorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSensorController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+
+}
