a6e75e7073c60ab5467a2380c85c22f13b111730
RYMND1875 <aiminzoku@gmail.com>
2019-12-18 00:59:03 +0900

    ユニットボタン実装
    ユニットの選択と生成を行うボタンの追加

---
+++ HackTower/Assets/Game/UI/Scripts/UnitButtonDirector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitButtonDirector : MonoBehaviour
+{
+    public List<GameObject> Units=new List<GameObject>();
+    GameObject ChosenUnit;
+    int unitsIndex = 0;
+    public GameObject UnitDisplayer;
+    void UnitDisplay(){
+        UnitDisplayer.GetComponent<Image>().sprite=ChosenUnit.GetComponent<SpriteRenderer>().sprite;
+    }
+    void Initial(){
+        ChosenUnit=Units[unitsIndex];
+        UnitDisplay();
+    }
+    
+    public void UnitChange()
+    {
+        if(Units.Count==0)Debug.Log("UnitListが空です");
+        unitsIndex+=1;
+        if(Units.Count<=unitsIndex)unitsIndex=0;
+        ChosenUnit = Units[unitsIndex];
+        UnitDisplay();
+        Debug.Log("現在の選択ユニットは"+ChosenUnit);
+    }
+    public void UnitCreate(){
+        Instantiate(ChosenUnit);
+    }
+
+    void Start()
+        {Initial();}
+    public void OnClickChange()
+        {UnitChange();}
+    public void OnClickCreate()
+        {UnitCreate();}
+    
+}
--- HackTower/Assets/Game/Units/Scripts/UnitController.cs
+++ HackTower/Assets/Game/Units/Scripts/UnitController.cs
@@ -3,36 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitController : MonoBehaviour
-{
-    public class UnitStatus{
-    int cost;
-    int hp;
-    int attackPow;
-    int hackPow;
-    float speed;
-    }
-    int selfRootIndex;
+{   
+    public int cost;
+    public int hp;
+    public int attackPow;
+    public int hackPow;
+    public float speed=0.02f;
+    float deltaSpeed;
     List<Vector3Int> root;
-    float defaultMoveCoolTime=1;
-    float moveCoolTime=1;
-    int rootIndex=0;
-    void Move(){
-        moveCoolTime-=Time.deltaTime;
-        if(moveCoolTime<0){
-            rootIndex++;
-            if(rootIndex>root.Count)Destroy(this.gameObject);
-            transform.position=root[rootIndex];
-            moveCoolTime=defaultMoveCoolTime;
-        }
-    
-    }
-   
-    
-    
+     
     
     void Initial(){
         root=TileRootMaker.RootList;
         this.gameObject.transform.position=root[0];
+    }
+    void Move(){
+        deltaSpeed+=speed;
+        transform.position= root[(int)deltaSpeed];
     }
 
     
